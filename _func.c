bc41654462ebe065f6cc2838d7ad2d593a36c6f8
Bless-dem <blessdem20@gmail.com>
2023-06-21 05:00:55 -0700

    func file

---
+++ _func.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * get_op_func - function to select correct operation function
+ * @token1: 1st bytecode input (opcode)
+ * Return: pointer to correct operation function
+ */
+void (*get_op_func(char *token1))(stack_t **stack, unsigned int line)
+{
+	instruction_t instruction_s[] = {
+		{"pop", pop},
+		{"pall", pall},
+		{"pint", pint},
+		{"swap", swap},
+		{"add", _add},
+		{"sub", _sub},
+		{"mul", _mul},
+		{"div", _div},
+		{"mod", _mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"nop", nop},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
+	int j = 0;
+
+	while (instruction_s[j].f != NULL)
+	{
+		if (strcmp(token1, instruction_s[j].opcode) == 0)
+			return (instruction_s[j].f);
+		j++;
+	}
+	return (NULL);
+}
+__func.c
+Displaying __func.c.
